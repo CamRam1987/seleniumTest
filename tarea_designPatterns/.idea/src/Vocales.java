250b9520da373aca08f3e499212a0c58a83ddfa1
Camilo Ramirez <camilo.alberto.ramirez@hotmail.com>
2022-04-13 21:50:03 -0500

    funcional1

---
+++ tarea_designPatterns/.idea/src/Vocales.java
@@ -0,0 +1,29 @@
+public class Vocales {
+    public static String cifradoNumerico(String textoacodificar){
+        char letra;
+        String frase2="";
+        for (int j = 0; j < textoacodificar.length(); j++) {
+            letra = textoacodificar.charAt(j);
+
+            if (letra=='a') {
+                letra = '1';
+            }
+            if (letra=='e') {
+                letra = '2';
+            }
+            if (letra=='i') {
+                letra = '3';
+            }
+            if (letra=='o') {
+                letra = '4';
+            }
+            if (letra=='u') {
+                letra = '5';
+            }
+            else {
+                frase2 = frase2+letra;
+            }
+        }
+        return frase2;
+    }
+}
---
+++ tarea_designPatterns/.idea/src/principal.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class principal {
+
+    public static void main(String[] args) throws IOException {
+        cesar cesarobject=new cesar();
+        Vocales vcobject=new Vocales();
+        File file=new File("C:\\Users\\camil\\OneDrive\\Documentos\\School of Automation/textinput.txt");
+        Scanner scan= new Scanner(file);
+        String textoInicial;
+        String[] array;
+        String textoacodificar;
+        String codificador;
+        while(scan.hasNextLine()){
+            textoInicial=scan.nextLine();
+            array=textoInicial.split("-");
+            codificador=array[0];
+            textoacodificar=array[1];
+            if(codificador.charAt(0)=='c'){
+                System.out.println("Texto cifrado Cesar: " + cesarobject.cifradoCesar(textoacodificar));
+                System.out.println("Texto Original: "+textoInicial);
+            }
+            if(codificador.charAt(0)=='v'){
+                System.out.println("Texto cifrado númerico: " + vcobject.cifradoNumerico(textoacodificar));
+                System.out.println("Texto Original: "+textoInicial);
+           }/*else{
+                System.out.println("texto no codificable");
+            }*/
+        }
+
+    }
+
+
+} //Fin cifrado Cesar
